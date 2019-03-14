c08f3e41892c6155a3b53bd0d89f1886f3131727
all4you <wh_all4you@hotmail.com>
2019-03-14 14:18:50 +0800

    增加nacos配置管理适配模块

--- nacos-config-adapter/src/main/java/com/lememo/nacosconfigadapter/ExampleNacosConfigService.java
+++ nacos-config-adapter/src/main/java/com/lememo/nacosconfigadapter/ExampleNacosConfigService.java
@@ -10,6 +10,14 @@ import java.util.List;
  * jdbc.password=fake-password
  * some.api.qps=1000
  * some.method.switch=true
+ * time.diff=3600000
+ * 如果直接在 nacos 中管理这些配置，用户会非常头疼：
+ * <ul>
+ *     <li>1.对于运营来说，诸如 jdbc.url 这种系统相关的配置项，他们不需要关心，也不应该由他们来管理这些配置项，直接暴露给他们的话，会是一个很危险的事情</li>
+ *     <li>2.有些配置项无法仅从 key 的命名就判断出该配置项的意思，因为他们并不能做到那么好的自解释，例如上述配置中的 time.diff 就很具有疑惑性</li>
+ *     <li>3.配置的修改应该做到最小化，如果将所有的配置项全部暴露，用户在修改时手误将其中某一个配置项删除了，将会带来灾难性的后果</li>
+ *     <li>4.不同的人根据自己的权限，只需要负责自己所能修改的配置项即可，不应该将所有配置项全部暴露</li>
+ * </ul>
  **/
 public class ExampleNacosConfigService extends AbstractNacosConfigService {
 
@@ -56,10 +64,18 @@ public class ExampleNacosConfigService extends AbstractNacosConfigService {
         factor4.setType(NacosConfigType.BOOLEAN);
         factor4.setGrade(2);
 
+        NacosConfigFactor factor5 = new NacosConfigFactor();
+        // time.diff
+        factor5.setKey("time.diff");
+        factor5.setDesc("时间差，单位：毫秒");
+        factor5.setType(NacosConfigType.INT);
+        factor5.setGrade(2);
+
         factorList.add(factor1);
         factorList.add(factor2);
         factorList.add(factor3);
         factorList.add(factor4);
+        factorList.add(factor5);
 
         return factorList;
     }
